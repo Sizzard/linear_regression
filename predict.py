4b9e3282532bc129fbd13613fa42cf5de7ba58e8
Facarval <facarval@student.42.fr>
2025-03-22 08:18:14 +0100

    Done

--- predict.py
+++ predict.py
@@ -1,21 +1,43 @@
+import numpy as np
+import pandas as pd
+
+
+def getDataSet(filepath: str) -> any:
+    df = pd.read_csv(filepath)
+    return df
+
+
+def getData():
+    df = getDataSet("data.csv")
+    km = df.iloc[:, 0]
+    price = df.iloc[:, 1]
+    km = np.array(km)
+    price = np.array(price)
+    return km, price
+
+
 def getThetas():
     with open("thetas.txt", 'r') as file:
         data = file.read()
         thets = data.split('\n')
-        thet0 = float(thets[0])
-        thet1 = float(thets[1])
-        return thet0, thet1
+        t0 = float(thets[0])
+        t1 = float(thets[1])
+        return t0, t1
 
-def estimate(mileage: int, thet0: float, thet1: float) -> float:
-    result = thet0 + (thet1 * mileage)
+def estimate(mileage, t0, t1):
+    result = t0 + (t1 * mileage)
     return result
 
 def main():
     try:
         str = input("Mileage : ")
         mileage = int(str)
-        thet0, thet1 = getThetas()
-        result = estimate(mileage, thet0, thet1)
+        t0, t1 = getThetas()
+        km, _ = getData()
+        km = np.append(km, mileage)
+        km_norm = (km - km.mean()) / km.std()
+        mileage = km_norm[-1]
+        result = estimate(mileage, t0, t1)
         print(result)
     except Exception as e:
         print(e)
--- train.py
+++ train.py
@@ -1,153 +1,60 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from predict import estimate
-from predict import getThetas
+from predict import estimate, getData
 
 
-def getDataSet(filepath: str) -> any:
-    df = pd.read_csv(filepath)
-    return df.iloc[:, 1], df.iloc[:, 0]
+def show_plot(km, price, estimated):
+    plt.scatter(km, price)
+    plt.xlabel("Experience")
+    plt.ylabel("Salary")
+    plt.plot(km, estimated, color="red")
+    plt.show()
 
 
-# def compute_theta0(price, km, th0: float, th1: float, learning_rate: float):
-#     error = 0
-#     for idx, _ in enumerate(km):
-#         error += float(estimate(km[idx], th0, th1) - price[idx])
-#     summation = (error / km.size)
-#     return th0 - summation * learning_rate
-
-
-# def compute_theta1(price, km, th0: float, th1: float, learning_rate: float):
-#     error = 0
-#     for idx, _ in enumerate(km):
-#         error += float(estimate(km[idx], th0, th1) - price[idx]) * km[idx]
-#     summation = (error / km.size)
-#     return th1 - summation * learning_rate
-
-
-# def compute_alt_0(th0, errors, learning_rate):
-#     return th0 - (learning_rate / len(errors)) * sum(errors)
-
-
-# def compute_alt_1(th1, km, errors, learning_rate):
-#     res = np.dot(errors, km)
-#     res = (learning_rate / len(errors)) * res
-#     return th1 - res
-
-
-def compute_errors(price, estimated) -> any:
-    return [estimated[idx] - price[idx] for idx in range(len(price))]
-
-
-def compute_estimated(km, th0, th1) -> any:
-    return [th0 + th1 * km[idx] for idx in range(len(km))]
-
-
-# def show_plot(price, km, th0: float, th1: float):
-#     x_pred = estimate(km, th0, th1)
-#     print(np.column_stack((km, x_pred)))
-#     plt.xlabel(km.name)
-#     plt.ylabel(price.name)
-#     plt.scatter(km, price, label='Data Points')
-#     plt.plot(km, x_pred, color='red')
-#     plt.show()
-
-
-# def real_polyfit(price, km):
-#     coefficients = np.polyfit(price, km, 1)
-#     print(coefficients)
-#     p = np.poly1d(coefficients)
-#     plt.scatter(price, km, label='Data Points')
-#     plt.plot(price, p(price), label='Linear Fit', color='red')
-#     plt.legend()
-#     plt.show()
-
-# def algorithm():
-#     price, km = getDataSet('data.csv')
-#     tmp0, tmp1 = getThetas()
-#     learning_rate = 0.0000000001
-#     print(tmp0, tmp1)
-#     for _ in range(1500):
-#         estimated = compute_estimated(km, tmp0, tmp1)
-#         errors = compute_errors(price, estimated)
-#         tmp0 = compute_theta0(price, km, tmp0, tmp1, learning_rate)
-#         tmp1 = compute_theta1(price, km, tmp0, tmp1, learning_rate)
-#         tmp0 = compute_alt_0(tmp0, errors, learning_rate)
-#         tmp1 = compute_alt_1(tmp1, km, errors, learning_rate)
-#         print(f"0 = {tmp0}")
-#         print(f"1 = {tmp1}")
-#     print(estimated)
-#     print(tmp0)
-#     print(tmp1)
-
-#     show_plot(price, km, tmp0, tmp1)
-
-
-price, temp = getDataSet("set.csv")
-
-temp = np.array(temp)
-
-price = np.array(price)
-
-
-def calc_thet1(thet1: float, learning_rate: float, errors: list) -> float:
-
-    res = 0
-    for idx, _ in enumerate(errors):
-        res += errors[idx] * temp[idx]
-    res = res / len(errors)
-    print(f"res : {res}")
-    res = res * learning_rate
-    thet1 = thet1 - res
-
-    return thet1
-
-
-def algorithm():
-    thet0 = 0
-    thet1 = 0
-    learning_rate = 0.0001
-    iterations = 5000
-    estimated = [0, 0, 0]
-    errors = [0, 0, 0]
-    cost_history = []
+def algorithm(km, price):
+    t0 = 0
+    t1 = 0
+    km_norm = (km - km.mean()) / km.std()
+    m = len(km_norm)
+    iterations = 10000
+    learning_rate = 0.01
+    estimated = []
     for _ in range(iterations):
-        # estimated[0] = estimate(temp[0], thet0, thet1)
-        # estimated[1] = estimate(temp[1], thet0, thet1)
-        # estimated[2] = estimate(temp[2], thet0, thet1)
-        estimated = compute_estimated(temp, thet0, thet1)
-        print(f"Iteration : {_} : {estimated}")
-        # for idx, _ in enumerate(estimated):
-        #     errors[idx] = estimated[idx] - price[idx]
-        errors = compute_errors(price, estimated)
-        print("errors : ", errors)
-        thet0 = thet0 - (sum(errors) / len(errors)) * learning_rate
-        thet1 = calc_thet1(thet1, learning_rate, errors)
+        tmp0 = t0
+        tmp1 = t1
+        sum0 = 0
+        sum1 = 0
+        
+        for i in range(m):
+            error = (estimate(km_norm[i], tmp0, tmp1) - price[i])
+            sum0 += (error)
+            sum1 += (error * km_norm[i])
+        
+        d0 = (1 / m) * sum0
+        d1 = (1 / m) * sum1
 
-        print(thet0, thet1)
-        guessed = estimate(88, thet0, thet1)
-        print(f"Prix d'une glace quand il fait 88 degree : {guessed}")
-        cost = sum(e**2 for e in errors) / (2 * len(errors))
-        cost_history.append(cost)
+        t0 = tmp0 - (learning_rate * d0)
+        t1 = tmp1 - (learning_rate * d1)
+        if _ % 500 == 0 or _ == 1 or _ == 0:
+            print(t0, t1)
+            print(f"sum0 : {sum0}, sum1 : {sum1}")
 
-    plt.plot(range(iterations), cost_history, color='blue')
-    plt.xlabel("Itérations")
-    plt.ylabel("Coût J(θ)")
-    plt.title("Évolution de la fonction de coût")
-    plt.show()
-    print("estimated : ", estimated)
+    for mileage in km_norm:
+        estimated.append(estimate(mileage, t0, t1))
 
-    pred = thet0 + thet1 * temp
-    print(pred)
-    plt.scatter(temp, price)
-    plt.plot(temp, pred, color='r')
-    plt.show()
+    show_plot(km, price, estimated)
 
+    return t0, t1
+
+
+def writeThetas(t0, t1):
+    with open("thetas.txt" , 'w') as file:
+        file.write(f"{t0}\n{t1}")
 
 def main():
     try:
-        algorithm()
+        km, price = getData()
+        t0, t1 = algorithm(km, price)
+        writeThetas(t0, t1)
     except Exception as e:
         print(e)
 
